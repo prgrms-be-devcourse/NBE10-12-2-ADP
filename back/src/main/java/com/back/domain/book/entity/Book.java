648172f3bf9eed4547d3ef8aae9111ec2c870a94
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2026-06-29 16:44:03 +0900

    [feat/ADP-46] Book 엔티티 추가 (https://github.com/ALLDAY-PROJECT-dev/READTHEM.md/pull/5)
    * feat: Book 엔티티 추가
    
    * fix: String에서 LocalDateTime으로 타입 변경
    
    ---------
    
    Co-authored-by: mjkim12345 <zmdl96@gmail.com>

--- back/src/main/java/com/back/domain/book/entity/Book.java
+++ back/src/main/java/com/back/domain/book/entity/Book.java
@@ -1,9 +1,42 @@
 package com.back.domain.book.entity;
 
 import com.back.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Book extends BaseEntity {
-    //
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(unique = true)
+    private String isbn;
+
+    private String authors;
+
+    private LocalDateTime publishedDate;
+
+    private String publisher;
+
+    private String imgUrl;
+
+    public Book(String title, String description, String isbn,
+                String authors, LocalDateTime publishedDate,
+                String publisher, String imgUrl) {
+        this.title = title;
+        this.description = description;
+        this.isbn = isbn;
+        this.authors = authors;
+        this.publishedDate = publishedDate;
+        this.publisher = publisher;
+        this.imgUrl = imgUrl;
+    }
 }
