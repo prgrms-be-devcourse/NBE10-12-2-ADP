a7ebe7e353f261aac6e5fa9989128c17d2655a95
puppywimy <74778253+puppywimy@users.noreply.github.com>
2026-06-30 16:16:38 +0900

    [feat/ADP-9] GET /widgets/{memberId} - 위젯 조회 (https://github.com/ALLDAY-PROJECT-dev/READTHEM.md/pull/16)
    * test: 위젯 조회 테스트 코드 추가
    
    * fix: ApiV1WidgetController RequestMapping을 올바르게 수정 "/members" -> "/widgets"
    
    * test: 변경된 위젯 API 명세에 맞게 테스트 코드 수정, memberId -> githubId

--- back/src/main/java/com/back/domain/widget/controller/ApiV1WidgetController.java
+++ back/src/main/java/com/back/domain/widget/controller/ApiV1WidgetController.java
@@ -6,8 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/v1/widgets")
 @RequiredArgsConstructor
 public class ApiV1WidgetController {
     private final WidgetService widgetService;
+
+    public void getWidget() {
+        // 구현 필요
+    }
 }
