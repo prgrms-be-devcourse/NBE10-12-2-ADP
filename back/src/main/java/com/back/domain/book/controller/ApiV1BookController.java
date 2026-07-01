3994ef9a97c941caad8b0bd4245f8c2a485149dc
EasyH <skysea001010@gmail.com>
2026-07-01 15:14:49 +0900

    feat: ApiV1BookController 설명 추가

--- back/src/main/java/com/back/domain/book/controller/ApiV1BookController.java
+++ back/src/main/java/com/back/domain/book/controller/ApiV1BookController.java
@@ -2,6 +2,8 @@ package com.back.domain.book.controller;
 
 import com.back.domain.book.dto.BookDto;
 import com.back.domain.book.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +14,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
+@Tag(name = "ApiV1BookController", description = "API 도서 컨트롤러")
 public class ApiV1BookController {
     private final BookService bookService;
 
     @GetMapping
+    @Operation(summary = "도서 다건 조회")
     public List<BookDto> getBooks() {
         return bookService.getBooks();
     }
