15a09b38acfefd597e6dfdb35d23ca0c40477b07
mjkim12345 <144995949+mjkim12345@users.noreply.github.com>
2026-07-01 16:30:44 +0900

    [Feat/ADP-75] 도서 검색 API 구현 (#11)
    Co-authored-by: mjkim12345 <zmdl96@gmail.com>

--- back/src/main/java/com/back/domain/book/repository/BookRepository.java
+++ back/src/main/java/com/back/domain/book/repository/BookRepository.java
@@ -2,7 +2,10 @@ package com.back.domain.book.repository;
 
 import com.back.domain.book.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
+
+    List<Book> findByTitleContaining(String searchTerm);
 }
