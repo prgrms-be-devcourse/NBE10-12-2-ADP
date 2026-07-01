99e424affde543612592ea1bf5e7b7d92390c439
puppywimy <eric250@naver.com>
2026-07-01 13:02:34 +0900

    feat(front): 프로젝트 생성(기존 프로젝트 활용, .gitignore env 설정 및 .env.example 추가)

---
+++ front/src/app/layout.tsx
@@ -0,0 +1,49 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "사이트 A",
+  description: "스프링부트, Next.js 연동",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="ko"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      <body className="flex flex-col min-h-screen">
+        <header>
+          <nav className="flex">
+            <Link href="/" className="p-2 rounded hover:bg-gray-100">
+              메인
+            </Link>
+            <Link href="/posts" className="p-2 rounded hover:bg-gray-100">
+              글 목록
+            </Link>
+          </nav>
+        </header>
+        <main className="flex-grow flex flex-col p-2">{children}</main>
+        <footer className="text-center p-2">푸터</footer>
+      </body>
+    </html>
+  );
+}
---
+++ front/src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Page() {
+  return (
+    <div className="flex-grow flex items-center justify-center">
+      안녕하세요.
+    </div>
+  );
+}
