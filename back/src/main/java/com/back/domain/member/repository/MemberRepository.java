97c73cc27782bfef57e00cdf3252c4bb431120cb
puppywimy <74778253+puppywimy@users.noreply.github.com>
2026-07-01 14:26:56 +0900

    [feat/ADP-76] [MemberService] findByGithubId 메서드 구현 (#5)

--- back/src/main/java/com/back/domain/member/repository/MemberRepository.java
+++ back/src/main/java/com/back/domain/member/repository/MemberRepository.java
@@ -9,4 +9,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByRefreshToken(String apiKey);
 
     Optional<Member> findByUsername(String username);
+
+    Optional<Member> findByGithubId(String githubId);
 }
