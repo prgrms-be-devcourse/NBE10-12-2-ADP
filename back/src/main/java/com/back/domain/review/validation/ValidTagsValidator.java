becd07f0fa5410e4fcf298e5316a26b113d03c87
puppywimy <eric250@naver.com>
2026-07-02 18:23:04 +0900

    temp

--- back/src/main/java/com/back/domain/review/validation/ValidTagsValidator.java
+++ back/src/main/java/com/back/domain/review/validation/ValidTagsValidator.java
@@ -3,15 +3,37 @@ package com.back.domain.review.validation;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class ValidTagsValidator implements ConstraintValidator<ValidTags, List<String>> {
     @Override
     public boolean isValid(List<String> value, ConstraintValidatorContext context) {
         if (value == null) return true;
 
-        // TODO
+        return isMaxSize5() && isAllValidTag
 
-        return true;
+        Map<String, Integer> tagCounts = new HashMap<>();
+
+        boolean isMaxSize5 = value.size() <= 5;
+        boolean isAllTagValid = true;
+        boolean isAllTagUnique = true;
+
+        for (String tag : value) {
+            boolean isValid = 1 <= tag.length() && tag.length() <= 20;
+
+            tagCounts.put(tag, Optional.ofNullable(tagCounts.get(tag)).orElse(0) + 1);
+        }
+        for (int count : tagCounts.values()) {
+            isAllTagUnique = count <= 1;
+        }
+
+        tagCounts.forEach((_, count) -> {
+            if (count > 1) isAllTagUnique = true;
+        });
+
+        return isMaxSize5 && isAllTagValid;
     }
 }
