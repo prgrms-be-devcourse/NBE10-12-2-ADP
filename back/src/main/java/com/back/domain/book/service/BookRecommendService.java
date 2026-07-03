9ae32cf241041eef65d6688627e3995f06c52385
EasyH <skysea001010@gmail.com>
2026-07-03 20:00:33 +0900

    feat: 모든 리뷰 대상으로 추천 -> 최근 리뷰와 최근 리뷰 도서 평가자를 이용하여 최적화

--- back/src/main/java/com/back/domain/book/service/BookRecommendService.java
+++ back/src/main/java/com/back/domain/book/service/BookRecommendService.java
@@ -2,28 +2,67 @@ package com.back.domain.book.service;
 
 import com.back.domain.book.dto.BookDto;
 import com.back.domain.member.entity.Member;
+import com.back.domain.review.entity.Review;
 import com.back.domain.review.service.ReviewService;
-import com.back.standard.recommend.byReview.CosineSimilarityRecommendByReview;
 import com.back.standard.recommend.byReview.RecommendByReview;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import static com.back.standard.recommend.byReview.RecommendByReview.RecommendReview;
+
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class BookRecommendService {
-    private final RecommendByReview recommendSystem = new CosineSimilarityRecommendByReview();
+    private final RecommendByReview recommendSystem = new RecommendByReview();
     private final ReviewService reviewService;
     private final BookService bookService;
 
+    private RecommendReview reviewToRecommendReview(Review review) {
+        return new RecommendReview(
+                review.getReviewer().getId(),
+                review.getBook().getId(),
+                review.getRating());
+    }
+
+    private List<RecommendReview> recommendReviewsByReviewer(Member reviewer) {
+
+        return reviewService.getByMember(reviewer, 0, 5)
+                .stream()
+                .map(this::reviewToRecommendReview)
+                .toList();
+
+    }
+
     public List<BookDto> getRecommends(Member actor) {
-        recommendSystem.setData(reviewService.getPureReviewAll()
-                .stream().map(review -> new RecommendByReview
-                        .RecommendReview(review.getReviewer().getId(),
-                        review.getBook().getId(), review.getRating())));
+
+        recommendSystem.clear();
+
+        List<Review> recentReviews = reviewService
+                .getByMember(actor, 0, 5)
+                .stream()
+                .toList();
+
+        recommendSystem.setData(
+                recommendReviewsByReviewer(actor));
+
+        Set<Member> members = new HashSet<>();
+
+        for (var review : recentReviews)
+            reviewService.getByBookId(review.getBook().getId(), 0, 10)
+                    .stream()
+                    .forEach(r -> members.add(r.getReviewer()));
+
+        for (var reviewer: members) {
+            recommendSystem.setData(
+                    recommendReviewsByReviewer(reviewer));
+        }
 
         return recommendSystem.getRecommend(actor.getId(), 5, 10)
+                .stream()
                 .map(id -> new BookDto(bookService.getPureBook(id)))
                 .toList();
     }
