001fbe82b849fbefc4d33a987663cade7ffc29f2
EasyH <skysea001010@gmail.com>
2026-07-06 17:23:08 +0900

    feat: 리뷰수 기반 집계, 평균 평점을 기반으로 정렬, 반환하는 함수 추가

--- back/src/main/java/com/back/domain/book/repository/BookRepository.java
+++ back/src/main/java/com/back/domain/book/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package com.back.domain.book.repository;
 
 import com.back.domain.book.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
@@ -8,4 +10,6 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByIsbn(String isbn);
 
     List<Book> findByTitleContaining(String searchTerm);
+
+    Page<Book> findAllByOrderByAverageRatingDesc(Pageable pageable);
 }
